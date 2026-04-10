cd98011230b8701acca0ec9031b606e7ec1afafd
Shangdi Yu <shangdiy@meta.com>
2026-04-09 17:31:10 -0700

    Make Generator an Opaque Object
    ghstack-source-id: 68d6a28c50eee66fc126f054b3c3e881af5e9668
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/179661

--- test/test_cpp_extensions_aot.py
+++ test/test_cpp_extensions_aot.py
@@ -11,12 +11,7 @@ import torch.backends.cudnn
 import torch.testing._internal.common_utils as common
 import torch.utils.cpp_extension
 from torch.testing._internal.common_cuda import TEST_CUDA
-from torch.testing._internal.common_utils import (
-    IS_WINDOWS,
-    skipIfTorchDynamo,
-    TEST_XPU,
-    xfailIfTorchDynamo,
-)
+from torch.testing._internal.common_utils import IS_WINDOWS, skipIfTorchDynamo, TEST_XPU
 
 
 try:
@@ -370,7 +365,6 @@ class TestRNGExtension(common.TestCase):
     def setUp(self):
         super().setUp()
 
-    @xfailIfTorchDynamo
     def test_rng(self):
         fourty_two = torch.full((10,), 42, dtype=torch.int64)
 
